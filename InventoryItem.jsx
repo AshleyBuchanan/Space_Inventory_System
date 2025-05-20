aca6758a3a863b35bcebdd70b3bcf248e5125fd2
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-05-20 11:18:52 -0700

    finished exercise. restrained myself from too much styling. :-P

--- InventoryItem.jsx
+++ InventoryItem.jsx
@@ -1,10 +1,18 @@
-function InventoryItem (/* TODO: Take the props. Set defaults to the quantity. */)
+function InventoryItem ({name, type, quantity = 0, price = 0.00})
 {
 	return (
 		<div>
-			<h2><!-- TODO: Render the item's details. --></h2>
-			<!-- TODO: Render the low stock alert based on the quantity of the item. -->
-			<!-- TODO: Render the high value alert based on the total value of the item. -->
+			<h2><span id="name">{name}</span> <span id="type">({type})</span></h2>
+
+                {/* <!-- low stock alert. --> */}
+                {quantity < 5 && (
+                    <Message><span className="icon">🚨</span> Low Stock! Only {quantity} remaining.</Message>
+                )}
+
+                {/* <!-- high value alert. --> */}
+                {quantity * price > 1000.00 && (
+                    <Message><span className="icon">💎</span> High Value. Consider extra protection!</Message>
+                )}
 		</div>
 	);
 }
